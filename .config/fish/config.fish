435896edbfabc455e3b7d068bcc3756e0bd87091
paltoslp <motata.frochaux@gmail.com>
2023-02-15 20:58:56 +0100

    kinda initial

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr --add n nvim
+alias c="clear"
+alias cdinit="cd ~/.config/nvim/"
+
+function starship_transient_prompt_func
+  starship module directory
+end
+starship init fish | source
+enable_transience
